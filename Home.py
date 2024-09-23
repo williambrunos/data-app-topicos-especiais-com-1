c25010c218ba02758287d75d630d60affa9f4dec
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-09-23 08:37:50 -0300

    feat: about the app dialog

--- Home.py
+++ Home.py
@@ -8,9 +8,41 @@ from analysis.acidentes_por_br import plot_acidentes_por_br
 from analysis.acidentes_por_clima import plot_acidentes_por_clima
 from analysis.causas_acidentes import plot_causas_acidentes
 
+def print_students():
+    text = "Alunos - Matrícula:\n"
+
+    students_names = [
+        "Gabriel Vasconcelos Santos - 497688",
+        "Izaias Machado Pessoa Neto - 497372",
+        "Márcio Bruno Loiola Gomes - 473740",
+        "Vinicius Costa dos Santos - 473003",
+        "Vitor Hugo Muniz de Sousa Santos - 475767",
+        "Wendel Manfrini de Andrade Mendes - 494899",
+        "William Bruno Sales de Paula Lima - 497345"
+    ]
+
+    for i, student in enumerate(students_names):
+        text += f"{i + 1}. {student}\n"
+
+    st.write(text)
+
+@st.dialog("Sobre o app")
+def about_the_app():
+    st.write("Este é um data app para análise de dados de acidentes de trânsito no Brasil.")
+    
+    st.write("O dataset utilizado foi o de [Acidentes 2024 (Agrupados por ocorrência)](https://www.gov.br/prf/pt-br/acesso-a-informacao/dados-abertos/dados-abertos-da-prf) disponibilizado pela Polícia Rodoviária Federal no portal de dados abertos do governo.")
+
+    st.write("### Equipe 02 - Trabalho 02 - Ciência de Dados | UFC 2024.1")
+
+    print_students()
+
 st.sidebar.markdown("Bem vindo ao data app do Data Tran 2024")
-st.write("# Data App - Tópicos especiais em computação 1 \n"
-         "## Dados DataTran 2024")
+
+st.write("# Data App - Ciência de Dados | UFC 2024.1")
+
+st.write("Este é um data app para análise de dados de acidentes de trânsito no Brasil. Para mais informações, clique no botão \"Sobre o app\" no menu lateral.")
+
+st.write("## Dados DataTran 2024")
 
 dados = pd.read_csv('./data/datatran2024_limpo.csv', sep=';')
 
@@ -30,4 +62,6 @@ option = st.sidebar.selectbox(
 if option:
     factory_analise[option](dados)
 
-st.write("### Obrigado!!")
+if "about_the_app" not in st.session_state:
+    if st.sidebar.button("Sobre o app"):
+        about_the_app()
