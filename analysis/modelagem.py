7922bc7d28a802fa287b3b98e4e1355fc87486b2
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-09-24 22:41:54 -0300

    fix: feature importance graphs

--- analysis/modelagem.py
+++ analysis/modelagem.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import altair as alt
+import matplotlib.pyplot as plt
 
 def plot_modelagem(dados):
     dados_acuracia = pd.read_csv('./data/models_accuracy.csv')
@@ -12,7 +13,7 @@ def plot_modelagem(dados):
     st.bar_chart(dados_acuracia_classificacao_acidente, x='model', y='accuracy', horizontal=False, x_label="Acurácia", y_label="Modelo")
 
     dados_classificacao_acidente = dados_feature_importance[dados_feature_importance['prediction_feature'] == 'classificacao_acidente']
-    st.bar_chart(dados_classificacao_acidente, x='feature', y='feature_importance', horizontal=True, x_label="Importância da feature", y_label="Nome da feature")
+    plot_feature_importance(dados_classificacao_acidente, 'Classificação do Acidente')
 
     st.write("## Predição da quantidade de feridos")
 
@@ -20,7 +21,7 @@ def plot_modelagem(dados):
     st.bar_chart(dados_acuracia_feridos_graves, x='model', y='accuracy', horizontal=False, x_label="Acurácia", y_label="Modelo")
 
     dados_feridos_graves = dados_feature_importance[dados_feature_importance['prediction_feature'] == 'feridos_graves']
-    st.bar_chart(dados_feridos_graves, x='feature', y='feature_importance', horizontal=True, x_label="Importância da feature", y_label="Nome da feature")
+    plot_feature_importance(dados_feridos_graves, 'Feridos Graves')
 
     st.write("## Predição do tipo de pista")
 
@@ -28,4 +29,12 @@ def plot_modelagem(dados):
     st.bar_chart(dados_acuracia_tipo_pista, x='model', y='accuracy', horizontal=False, x_label="Acurácia", y_label="Modelo")
 
     dados_tipo_pista = dados_feature_importance[dados_feature_importance['prediction_feature'] == 'tipo_pista']
-    st.bar_chart(dados_tipo_pista, x='feature', y='feature_importance', horizontal=True, x_label="Importância da feature", y_label="Nome da feature")
+    plot_feature_importance(dados_tipo_pista, 'Tipo de Pista')
+
+def plot_feature_importance(dados, target_name):
+    fig = plt.figure(figsize=(10, 6))
+    plt.barh(dados['feature'], dados['feature_importance'], color='skyblue')
+    plt.xlabel('Importância')
+    plt.title(f'Importância das Características ({target_name})')
+    # plt.show()
+    st.pyplot(fig)
